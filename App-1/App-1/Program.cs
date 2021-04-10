715c9563913f8b145fc427fae6d9a221dbdec4d1
kienz7777 <kienvu7777@gmail.com>
2021-04-10 21:46:50 +0700

    first commit

---
+++ App-1/App-1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace App_1
+{
+    class Program
+    {
+        public static int SumOfArray(int[] arr)
+        {
+            int sum = 0;
+            foreach (int item in arr)
+            {
+                sum += item;
+            }
+            return sum;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] cars = { 1, 2, 3, 4, 5 };
+
+            int kq = SumOfArray(cars);
+
+            Console.WriteLine(kq);
+
+        }
+    }
+}
